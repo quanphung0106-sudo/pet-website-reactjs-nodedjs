04eed488463110bda6bc54889cadfd74614eabcc
quanphung0106-sudo <phungdinhquan16@gmail.com>
2022-05-05 05:06:06 +0700

    initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PhoneEnabledOutlinedIcon from '@mui/icons-material/PhoneEnabledOutlined';
+
+function App() {
+  return (
+    <div className="App">
+      <PhoneEnabledOutlinedIcon />
+    </div>
+  );
+}
+
+export default App;
