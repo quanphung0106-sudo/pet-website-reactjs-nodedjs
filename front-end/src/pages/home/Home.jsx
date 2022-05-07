c8e2caea105f1d9026163f5c6dad0e18009ba3a6
quanphung0106-sudo <phungdinhquan16@gmail.com>
2022-05-07 16:10:50 +0700

    installed customize-cra package, babel-plugin-module-resolvers and formatting code with prettier

--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
-import Advantages from "../../components/advantages/Advantages";
-import Benefits from "../../components/benefits/Benefits";
-import Contact from "../../components/contact/Contact";
-import DataList from "../../components/datalist/DataList";
-import Featured from "../../components/featured/Featured";
-import Footer from "../../components/footer/Footer";
-import Navbar from "../../components/navbar/Navbar";
-import ScrollToTop from "../../components/scrollToTop/ScrollToTop";
+import Advantages from '~/components/advantages/Advantages';
+import Benefits from '~/components/benefits/Benefits';
+import Contact from '~/components/contact/Contact';
+import DataList from '~/components/datalist/DataList';
+import Featured from '~/components/featured/Featured';
+import Footer from '~/components/footer/Footer';
+import Navbar from '~/components/navbar/Navbar';
+import ScrollToTop from '~/components/scrollToTop/ScrollToTop';
 
 const Home = () => {
   return (
