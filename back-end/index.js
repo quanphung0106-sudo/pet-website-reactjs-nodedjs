5f2b17f153c096c49b46c591df8b98429c586e70
quanphung0106-sudo <phungdinhquan16@gmail.com>
2022-05-11 16:40:09 +0700

    deploying back-end to heroku

--- back-end/index.js
+++ back-end/index.js
@@ -7,7 +7,7 @@ const helmet = require('helmet');
 const route = require('./src/routes/index');
 
 dotenv.config();
-const port = process.env.PORT || 8800;
+const port = 8800;
 
 mongoose
   .connect(process.env.MONGO_URL, {
