4a8972d79e298e9268dcf098a5e1f4e04107f11f
quanphung0106-sudo <phungdinhquan16@gmail.com>
2022-05-16 08:50:15 +0700

    updating login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes/Routes';
-
 function App() {
   return (
     <div className="App">
--- front-end/src/pages/login/Login.jsx
+++ front-end/src/pages/login/Login.jsx
@@ -1,11 +1,15 @@
 import React, { useRef, useState } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Login.module.scss';
+
 import { Button, Container, Row, Col, Form, InputGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './login.css';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { CircularProgress } from '@mui/material';
 // import GitHubIcon from "@mui/icons-material/GitHub";
+
+const cx = classNames.bind(styles);
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
@@ -23,11 +27,31 @@ export default function Login() {
       username: username.current.value,
       password: password.current.value,
     };
+
+    console.log(userInfo);
   };
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className={cx('container')}>
+      <div className={cx('imgContainer')}>
+        <img src="/img/background-signin-signup.jpg" className={cx('backgroundImg')} alt="" />
+      </div>
+      <form className={cx('formWrapper')} onSubmit={handleSubmit} action="">
+        <h2 className={cx('title')}>Welcome back!</h2>
+        <div className={cx('texts')}>
+          <label htmlFor="username" className={cx('label')}>
+            Username
+          </label>
+          <input ref={username} className={cx('input')} type="text" id="username" placeholder="Your name" />
+        </div>
+        <div className={cx('texts')}>
+          <label htmlFor="password" className={cx('label')}>
+            Password
+          </label>
+          <input ref={password} className={cx('input')} type="text" id="password" placeholder="Your password" />
+        </div>
+        <button className={cx('button')}>Đăng nhập</button>
+      </form>
+    </div>
   );
 }
