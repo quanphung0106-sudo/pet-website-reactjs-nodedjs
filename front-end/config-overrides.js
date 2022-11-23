0e9e94b606411343fa255a8137bc3a7d1bd10e85
quanphung0106-sudo <phungdinhquan16@gmail.com>
2022-11-24 00:50:14 +0700

    deploying ...

--- front-end/config-overrides.js
+++ front-end/config-overrides.js
@@ -1,6 +1,12 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 /* config-overrides.js */
-const { override, useBabelRc } = require("customize-cra");
-const path = require("path");
+const { override, useBabelRc } = require('customize-cra');
+const path = require('path');
 
-module.exports = override(useBabelRc());
+module.exports = override(
+  useBabelRc({
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  }),
+);
